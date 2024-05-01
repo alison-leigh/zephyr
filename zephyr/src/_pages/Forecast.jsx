5a92b4533db978671a302659c170e418c8bca299
OhZenny <zarek.lacsamana@gmail.com>
2024-05-01 17:23:05 -0400

    Added 10 Day forecast page

--- zephyr/src/_pages/Forecast.jsx
+++ zephyr/src/_pages/Forecast.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Navigation from "../_components/Navigation.jsx";
+import Heading from '../_components/Heading.jsx';
 
 function Forecast(){
     // javascript used in page goes up here
@@ -8,8 +9,14 @@ function Forecast(){
         }
     );
     return(
-        <Navigation />
+        <>
+            <Navigation />
+            <div className='container bg-body-'>
+                <Heading title='10 Day Weather'/>
+            </div>
+        </>
         // jsx page structure stuff goes here
+        
     );
 }
 
