9ab0323765d56e1a8a26201026074ebe35849f14
OhZenny <zarek.lacsamana@gmail.com>
2024-05-03 00:13:04 -0400

    Applied bootstap to aq page

--- zephyr/src/_components/Card_AirQuality.jsx
+++ zephyr/src/_components/Card_AirQuality.jsx
@@ -1,27 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import '../styles/AirQuality.css';
-import { Textarea } from "@lemonsqueezy/wedges";
+import "../styles/AirQuality.css";
+import Heading from "./Heading";
+import { Input } from "@lemonsqueezy/wedges";
 
-
-function Card_AirQuality(){
-    return (
-        <div className="div-2">
-            <div className="left">
-                <div className="text-wrapper-3">Air Quality Score</div>
-                <div className="air-quality-card">
-                    <div className="text-wrapper-4">69</div>
-                    <div className="text-wrapper-5">Good</div>
-            </div>
+function Card_AirQuality() {
+  return (
+    <>
+      <Heading title={"Air Quality Score"}/>
+      <div className="row row-cols-1 d-flex align-items-center">
+          <Input label="Location" placeholder="Zip Code, City, or State" helperText="Find the AQ of any region" />
+        <div className="col-2 mx-auto ">
+          <div className="rounded p-4 bg-success text-center">
+            <div className="display-1 fw-bold text-white">79</div>
+            <div className="display-5 fw-semibold text-white">Good</div>
+          </div>
+          <div className="my-2">
+              <h3 className="h5">Kennsaw, GA</h3>
+              
+              <div className="">Atlanta Reporting Area</div>
+          </div>
         </div>
-            <div className="right">
-                <Textarea />
-
-                <div className="text-wrapper-5">Marietta, GA</div>
-                <div className="text-wrapper-6">Atlanta Reporting Area</div>
-            </div>
-        </div>
-    );
+      </div>
+    </>
+  );
 }
 
 export default Card_AirQuality;
--- zephyr/src/_pages/AirQuality.jsx
+++ zephyr/src/_pages/AirQuality.jsx
@@ -1,22 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import Navigation from "../_components/Navigation.jsx";
-import Card_AirQuality from '../_components/Card_AirQuality.jsx';
+import Card_AirQuality from "../_components/Card_AirQuality.jsx";
 
+function AirQuality() {
+  // javascript used in page goes up here
 
-function AirQuality(){
-    // javascript used in page goes up here
-
-    useEffect(() => {
-            document.title = "Zephyr | Air Quality";
-        }
-    );
+  useEffect(() => {
+    document.title = "Zephyr | Air Quality";
+  });
 
   return (
-    <div className="contentContainer">
+    <div className="">
       <Navigation />
+      <div className="container my-5 p-5 bg-body">
+
       <Card_AirQuality />
       </div>
+    </div>
   );
-};
+}
 
 export default AirQuality;
